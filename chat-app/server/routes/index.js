6d5315392026317434bcb4b10a9c69a7bc71e834
akahay <tech24into07@gmail.com>
2022-05-16 00:07:58 +0530

    5

--- chat-app/server/routes/index.js
+++ chat-app/server/routes/index.js
@@ -1,4 +1,13 @@
 const applyRoutes =(app) =>{
-    app.get('/',(req,res)=>res.send(`are bc ye to kam kr gya`))
+    app.get('/',(req,res)=>
+    res.send(`are bc ye to kam kr gya`))
+
+//creat//
+app.post('/user',(req,res)=>res.send(`creat user`));
+app.post('/login',(req,res)=>res.send(`login`));
+app.post('/channel',(req,res)=>res.send(`channel`));
+app.post('/search-user',(req,res)=>res.send(`serach user`));
+app.post('/channel list',(req,res)=>res.send(`channel list`));
+app.post('/message',(req,res)=>res.send(`message`));
 };
 export default applyRoutes;
